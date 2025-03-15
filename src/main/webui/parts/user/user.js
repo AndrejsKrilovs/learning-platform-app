36a07469c0a013d7cc453b1993d585dab4198038
Andrejs Krilovs <andrejs.krilovs.0507@gmail.com>
2025-03-15 23:37:55 +0200

    Frontend changes: finished login functionality refactor

--- src/main/webui/parts/user/user.js
+++ src/main/webui/parts/user/user.js
@@ -4,6 +4,7 @@ let passwordValue = ``
 const isEmptyString = (stringElement) => (!stringElement?.trim().length)
 
 const hidingLoginButton = () => {
+  document.querySelector("#incorrectCredentials").innerText = ``
   const loginBtn = document.querySelector('#login')
   if (loginValue && passwordValue) {
     loginBtn.removeAttribute('disabled')
@@ -27,13 +28,40 @@ const getPassword = () => {
   })
 }
 
+const clearFormData = () => {
+  loginValue = ``
+  passwordValue = ``
+  document.querySelector('#username').value = ``
+  document.querySelector('#password').value = ``
+  hidingLoginButton()
+}
+
 export const setupLogin = () => {
   getLogin()
   getPassword()
 
   const loginBtn = document.querySelector('#login')
   loginBtn.addEventListener('click', () => {
-    console.log('Logging....')
+    fetch('http://localhost:8080/users/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        login: loginValue,
+        password: passwordValue
+      })
+    })
+    .then(res => res.json())
+    .then(data => {
+      clearFormData()
+      if(data.login) {
+        console.log(data.login)
+      }
+      else {
+        document.querySelector("#incorrectCredentials").innerText = data.errorMessage
+      }
+    })
   })
 }
 
@@ -41,20 +69,19 @@ export const renderLoginForm = () => {
   return `
     <div class="login_form">
       <h3>Please login before continue</h3>
+      <span id="incorrectCredentials"></span>
 
-        <div class="input_box">
-          <label for="username">Username</label>
-          <span id="incorrectLogin" />
-          <input type="text" id="username" placeholder="Username required" required />
-        </div>
+      <div class="input_box">
+        <label for="username">Username</label>
+        <input type="text" id="username" placeholder="Username required" required />
+      </div>
 
-        <div class="input_box">
-          <label for="password">Password</label>
-          <span id="incorrectPassword" />
-          <input type="password" id="password" placeholder="Password required" required />
-        </div>
+      <div class="input_box">
+        <label for="password">Password</label>
+        <input type="password" id="password" placeholder="Password required" required />
+      </div>
 
-        <button id="login" type="button" disabled>Login</button>
+      <button id="login" type="button" disabled>Login</button>
     </div>
   `
 }
