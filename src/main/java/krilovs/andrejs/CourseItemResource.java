d0777b5a289774add713c62f5abe49b6770daf4d
Andrejs Krilovs <andrejs.krilovs.0507@gmail.com>
2025-04-04 19:26:53 +0300

    Backend changes: Empty all enpoints before refactoring and db implementation

--- src/main/java/krilovs/andrejs/CourseItemResource.java
+++ src/main/java/krilovs/andrejs/CourseItemResource.java
@@ -1,6 +1,5 @@
 package krilovs.andrejs;
 
-import jakarta.inject.Inject;
 import jakarta.validation.Valid;
 import jakarta.ws.rs.DefaultValue;
 import jakarta.ws.rs.GET;
@@ -12,36 +11,26 @@ import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import krilovs.andrejs.domain.CourseItemDomain;
 import krilovs.andrejs.exception.CourseException;
-import krilovs.andrejs.service.CourseItemService;
 
-import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Stream;
 
 @Path("/courseItems")
 @Produces(MediaType.APPLICATION_JSON)
 public class CourseItemResource {
   private final static int ITEM_COUNT_PER_PAGE = 10;
 
-  @Inject
-  private CourseItemService service;
-
   @GET
   public Map<String, ?> getCourseItems(@QueryParam("page") @DefaultValue("0") Integer pageNumber) {
-    int startIndex = pageNumber * ITEM_COUNT_PER_PAGE;
-    double totalPageCalculation = (double) service.totalElementCount() / ITEM_COUNT_PER_PAGE;
-    List<CourseItemDomain> courseItems = service.getCourseItems(startIndex, startIndex + ITEM_COUNT_PER_PAGE);
-
     Map<String, ?> metadata = Map.of(
-      "currentPage", pageNumber,
-      "totalPages", Double.valueOf(Math.ceil(totalPageCalculation)).intValue(),
-      "totalElements", courseItems.size()
+      "currentPage", 0,
+      "totalPages", 0,
+      "totalElements", 0
     );
 
     return Map.of(
-      "items", courseItems,
-      "headers", Stream.of(CourseItemDomain.class.getDeclaredFields()).map(Field::getName).toList(),
+      "items", List.of(),
+      "headers", List.of(),
       "metadata", metadata
     );
   }
@@ -49,22 +38,12 @@ public class CourseItemResource {
   @POST
   @Path("/add")
   public CourseItemDomain addCourseItem(@Valid CourseItemDomain courseItem) {
-    return service.addCourseItem(courseItem);
+    return courseItem;
   }
 
   @GET
   @Path("/take/{id}")
   public void takeCourse(@PathParam("id") Long courseId) {
-    if (service.getUserCourseItems().size() < ITEM_COUNT_PER_PAGE) {
-      service.takeCourse(courseId)
-        .orElseThrow(() -> new CourseException(
-          "Taking course exception",
-          "takeCourse",
-          "Selected course does not exists"
-        ));
-      return;
-    }
-
     throw new CourseException(
       "Taking course exception",
       "takeCourse",
@@ -75,12 +54,10 @@ public class CourseItemResource {
   @GET
   @Path("/userCourses")
   public Map<String, ?> getUserCourseItems() {
-    List<CourseItemDomain> userCourseList = service.getUserCourseItems();
-
     return Map.of(
-      "items", userCourseList,
-      "headers", Stream.of(CourseItemDomain.class.getDeclaredFields()).map(Field::getName).toList(),
-      "metadata", Map.of("totalElements", userCourseList.size())
+      "items", List.of(),
+      "headers",List.of(),
+      "metadata", Map.of("totalElements",0)
     );
   }
 }
